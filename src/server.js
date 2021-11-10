4c804b5572b9814be80a7af272b9f8593cd08878
Gabriel <g.pereira.a.m@gmail.com>
2021-11-10 09:19:19 -0300

    feat(Teste tecnico) - :sparkle: Configurando as variaveis de ambiente e seu uso no Model

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const { createController, updateController, readAllController, readByIdController, deleteController } = require('./controllers/motorsControllers');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.post('/webmotors', createController);
 app.get('/webmotors', readAllController);
@@ -12,4 +15,4 @@ app.get('/webmotors/:id', readByIdController);
 app.put('/webmotors/:id', updateController);
 app.delete('/webmotors/:id', deleteController);
 
-app.listen(3000, () => console.log('Web Motors ouvindo na porta 3000'));
+app.listen(PORT, () => console.log('Web Motors ouvindo na porta 3000'));
